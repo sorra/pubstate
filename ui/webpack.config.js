6badacc93d90038b69324ae339a71919fea2d702
Dongqing Hu <sorra@outlook.com>
2020-03-14 21:38:33 +0800

    Webpack environmental configuration

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,70 +2,84 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    main: './src/index.js'
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, '../src/main/webapp/static/dist')
-  },
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+module.exports = env => {
+  let isDev = env.development;
+  let isProd = env.production;
+
+  let minimizers = isProd ? [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})] : null;
+
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/index.js'
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, '../src/main/webapp/static/dist')
+    },
+    optimization: {
+      minimizer: minimizers,
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         }
-      },
-      {
-        test: /\.css$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          'file-loader'
-        ]
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        {
+          test: /\.css$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          include: path.resolve(__dirname, 'src'),
+          use: [
+            'file-loader'
+          ]
+        }
+      ]
+    },
+    devtool: isDev ? 'inline-source-map' : 'source-map',
+    plugins: [
+      new CleanWebpackPlugin(),
+      new ManifestPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[id].[contenthash].css'
+      })
     ]
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new ManifestPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[id].[contenthash].css'
-    })
-  ]
+  };
 };
+
+function optFilter(...objectsWithOptions) {
+  return objectsWithOptions.filter(c => c[1]).map(c => c[0]);
+}
